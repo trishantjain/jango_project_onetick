20845c7522ea196ed6a97d28b2a102117d09ff88
Trishant <trishantjain4444@gmail.com>
2022-11-26 09:45:52 +0530

    first commit

---
+++ firstproject/firstproject/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def test(request):
+
+    print("this is my first jango application")
+    return HttpResponse("<h1> this is my heading in jango framework </h1>")
+
+
+def homePage(request):
+    if request.method == 'POST':
+        varr = request.POST['uname']
+        print(varr)
+        varr = request.POST['urate']
+        print(varr)
+        varr = request.POST['uquan']
+        print(varr)
+    return render(request, "home.html")
+
+
+def regis(request):
+    return render(request, "regis.html")
+
+
+def formIndex(request):
+    return render(request, "index.html")
---
+++ firstproject/secondproject/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SecondprojectConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "secondproject"
